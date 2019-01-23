483c554b3e6d1492cd3689ff802f23c6927a056a
Kevin Mills <kevin.mills226@gmail.com>
2019-01-23 14:45:18 -0500

    Remove bs_ prefix.

--- battlescript.s
+++ battlescript.s
@@ -1,171 +1,171 @@
-.macro bs_goto, addr
+.macro goto, addr
     .byte 0x28
     .word addr
 .endmacro
 
-.macro bs_setmoveeffect, effect
+.macro setmoveeffect, effect
     .byte 0x2E
     .word effect_to_apply
     .byte effect
 .endmacro
 
-.macro bs_playanimation, bank, animation, var_address
+.macro playanimation, bank, animation, var_address
     .byte 0x45
     .byte bank
     .byte animation
     .word var_address
 .endmacro
 
-.macro bs_printstring, stringid
+.macro printstring, stringid
     .byte 0x10
     .halfword stringid
 .endmacro
 
-.macro bs_waitmessage, delay_
+.macro waitmessage, delay_
     .byte 0x12
     .halfword delay_
 .endmacro
 
-.macro bs_return
+.macro return
 	.byte 0x3c
 .endmacro
 
-.macro bs_call, address
+.macro call, address
 	.byte 0x41
 	.word address
 .endmacro
 
-.macro bs_orword, pointer, value
+.macro orword, pointer, value
 	.byte 0x35
 	.word pointer
 	.word value
 .endmacro
 
-.macro bs_healthbarupdate, bank
+.macro healthbarupdate, bank
 	.byte 0x0b
 	.byte bank
 .endmacro
 
-.macro bs_datahpupdate, bank
+.macro datahpupdate, bank
 	.byte 0x0c
 	.byte bank
 .endmacro
 
-.macro bs_updatestatusicon, battler
+.macro updatestatusicon, battler
 	.byte 0x98
 	.byte battler
 .endmacro
 
-.macro bs_printfromtable, ptr
+.macro printfromtable, ptr
 	.byte 0x13
 	.word ptr
 .endmacro
 
-.macro bs_seteffectprimary
+.macro seteffectprimary
 	.byte 0x16
 .endmacro
 
-.macro bs_statbuffchange, param0, param1
+.macro statbuffchange, param0, param1
 	.byte 0x89
 	.byte param0
 	.word param1
 .endmacro
 
-.macro bs_setbyte, ptr, param1
+.macro setbyte, ptr, param1
 	.byte 0x2e
 	.word ptr
 	.byte param1
 .endmacro
 
-.macro bs_setword, addr, value
+.macro setword, addr, value
 	.byte 0xFA
 	.word addr
 	.word value
 .endmacro
 
-.macro bs_msgbox, stringid
-	bs_printstring stringid
-	bs_waitmessage 0x40
+.macro msgbox, stringid
+	printstring stringid
+	waitmessage 0x40
 .endmacro
 
-.macro bs_playanimation_useitem, bank
-	bs_playanimation bank, 0x7, 0x0
+.macro playanimation_useitem, bank
+	playanimation bank, 0x7, 0x0
 .endmacro
 
-.macro bs_forcerandomswitch, param0
+.macro forcerandomswitch, param0
 	.byte 0x8F
 	.word param0
 .endmacro
 
-.macro bs_attackcanceler
+.macro attackcanceler
 	.byte 0x00
 .endmacro
 
-.macro bs_accuracycheck, p0, p1
+.macro accuracycheck, p0, p1
 	.byte 0x01
 	.word p0
 	.word p1
 .endmacro
 
-.macro bs_critcalc
+.macro critcalc
 	.byte 0x04
 .endmacro
 
-.macro bs_damagecalc
+.macro damagecalc
 	.byte 0x05
 .endmacro
 
-.macro bs_typecalc
+.macro typecalc
 	.byte 0x06
 .endmacro
 
-.macro bs_adjustnormaldamage
+.macro adjustnormaldamage
 	.byte 0x07
 .endmacro
 
-.macro bs_attackanimation
+.macro attackanimation
 	.byte 0x09
 .endmacro
 
-.macro bs_waitanimation
+.macro waitanimation
 	.byte 0x0A
 .endmacro
 
-.macro bs_effectivenesssound
+.macro effectivenesssound
 	.byte 0x0E
 .endmacro
 
-.macro bs_hitanimation, p0
+.macro hitanimation, p0
 	.byte 0x5C
 	.byte p0
 .endmacro
 
-.macro bs_waitstate
+.macro waitstate
 	.byte 0x3A
 .endmacro
 
-.macro bs_critmessage
+.macro critmessage
 	.byte 0x0D
 .endmacro
 
-.macro bs_resultmessage
+.macro resultmessage
 	.byte 0x0F
 .endmacro
 
-.macro bs_jumpifhasnohp, p0, p1
+.macro jumpifhasnohp, p0, p1
 	.byte 0xE3
 	.byte p0
 	.word p1
 .endmacro
 
-.macro bs_jumpifability, p0, p1, p2
+.macro jumpifability, p0, p1, p2
 	.byte 0x1E
 	.byte p0
 	.byte p1
 	.word p2
 .endmacro
 
-.macro bs_jumpifstatus3condition, p0, p1, p2, p3
+.macro jumpifstatus3condition, p0, p1, p2, p3
 	.byte 0x21
 	.byte p0
 	.word p1
@@ -173,22 +173,22 @@
 	.word p3
 .endmacro
 
-.macro bs_jumpiftype, battler, type, ptr
+.macro jumpiftype, battler, type, ptr
 	.byte 0x22
 	.byte battler
 	.byte type
 	.word ptr
 .endmacro
 
-.macro bs_end
+.macro end
 	.byte 0x3D
 .endmacro
 
-.macro bs_jumpifmovehadnoeffect, jumpptr
-	bs_jumpifbyte CMP_COMMON_BITS, b_attack_effectivity, MOVE_RESULT_NO_EFFECT, jumpptr
+.macro jumpifmovehadnoeffect, jumpptr
+	jumpifbyte CMP_COMMON_BITS, b_attack_effectivity, MOVE_RESULT_NO_EFFECT, jumpptr
 .endmacro
 
-.macro bs_jumpifbyte, ifflag, param1, param2, param3
+.macro jumpifbyte, ifflag, param1, param2, param3
 	.byte 0x29
 	.byte ifflag
 	.word param1
--- script.asm
+++ script.asm
@@ -1,27 +1,27 @@
 circle_throw_move_script:
-    bs_attackcanceler 
-    bs_accuracycheck 0x081D695E, 0x03020000 
-    bs_critcalc 
-    bs_damagecalc 
-    bs_typecalc 
-    bs_adjustnormaldamage 
-    bs_attackanimation 
-    bs_waitanimation 
-    bs_effectivenesssound 
-    bs_hitanimation 0x0 
-    bs_waitstate 
-    bs_healthbarupdate 0x0 
-    bs_datahpupdate 0x0 
-    bs_critmessage 
-    bs_waitmessage 0x40 
-    bs_resultmessage 
-    bs_waitmessage 0x40 
-    bs_jumpifhasnohp 0x0, 0x081D6947 
+    attackcanceler 
+    accuracycheck 0x081D695E, 0x03020000 
+    critcalc 
+    damagecalc 
+    typecalc 
+    adjustnormaldamage 
+    attackanimation 
+    waitanimation 
+    effectivenesssound 
+    hitanimation 0x0 
+    waitstate 
+    healthbarupdate 0x0 
+    datahpupdate 0x0 
+    critmessage 
+    waitmessage 0x40 
+    resultmessage 
+    waitmessage 0x40 
+    jumpifhasnohp 0x0, 0x081D6947 
     // handle type immunities
-    bs_jumpifmovehadnoeffect 0x081D6947
+    jumpifmovehadnoeffect 0x081D6947
     // ability 0x15 is Suction Cups
-    bs_jumpifability 0x0, 0x15, 0x081D9408 
+    jumpifability 0x0, 0x15, 0x081D9408 
     // status3 0x400 is rooted
-    bs_jumpifstatus3condition 0x0, 0x0400, 0x0, 0x081D8F27
-    bs_forcerandomswitch 0x081D7DF2 
-    bs_goto 0x081D6947 
+    jumpifstatus3condition 0x0, 0x0400, 0x0, 0x081D8F27
+    forcerandomswitch 0x081D7DF2 
+    goto 0x081D6947 
